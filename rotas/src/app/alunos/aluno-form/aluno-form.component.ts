2cdc4ffaf73f0e5564e7e5ff4d7d081133de066f
Edimar Junior <edimarbmjunior@gmail.com>
2019-09-30 14:25:59 -0300

    criando uma guarda de rota(canDeactivate) com interface generica

--- rotas/src/app/alunos/aluno-form/aluno-form.component.ts
+++ rotas/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { AlunosService } from '../alunos.service';
+import { IFormCanDeactivate } from 'src/app/guards/interfaceForm-canDeactivate';
 
-@Component({
+/* @Component({
   selector: 'app-aluno-form',
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
@@ -39,5 +40,47 @@ export class AlunoFormComponent implements OnInit {
 
   verificaPermissaoMudarRota():boolean{
     return this.formMudou;
+  }
+} */
+@Component({
+  selector: 'app-aluno-form',
+  templateUrl: './aluno-form.component.html',
+  styleUrls: ['./aluno-form.component.css']
+})
+export class AlunoFormComponent implements OnInit, IFormCanDeactivate {
+
+  aluno: any;
+  inscricao: Subscription;
+  private formMudou:boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private alunoService: AlunosService
+  ) { }
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+
+        this.aluno = this.alunoService.getAluno(id);
+        if(this.aluno === null){
+          this.aluno = {};
+        }
+      }
+    );
+  }
+
+  onInput(){
+    this.formMudou = true;
+    //console.log('aluno:', this.aluno);
+  }
+
+  verificaPermissaoMudarRota():boolean{
+    return this.formMudou;
+  }
+
+  podeDesativar(){
+    return this.verificaPermissaoMudarRota();
   }
 }
--- rotas/src/app/guards/alunos-deactivate.guard.ts
+++ rotas/src/app/guards/alunos-deactivate.guard.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AlunoFormComponent } from '../alunos/aluno-form/aluno-form.component';
+import { IFormCanDeactivate } from './interfaceForm-canDeactivate';
 
-@Injectable({providedIn: 'root'})
+/* @Injectable({providedIn: 'root'})
 export class AlunosDeactivateGuard implements CanDeactivate<AlunoFormComponent> {
 
   private verificaSairForm:boolean = false;
@@ -14,6 +15,31 @@ export class AlunosDeactivateGuard implements CanDeactivate<AlunoFormComponent> 
   ): Observable<boolean>|Promise<boolean>|boolean {
 
     if(alunoFormComponent.verificaPermissaoMudarRota()){
+
+      if(confirm('Quer realmente sair da página? Você perterá as alterações!')){
+        this.verificaSairForm = true;
+      }
+      if(this.verificaSairForm){
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+
+      return true;
+    }
+  }
+} */
+@Injectable({providedIn: 'root'})
+export class AlunosDeactivateGuard implements CanDeactivate<IFormCanDeactivate> {
+
+  private verificaSairForm:boolean = false;
+  canDeactivate(
+    alunoFormComponent: IFormCanDeactivate,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot
+  ): Observable<boolean>|Promise<boolean>|boolean {
+    if(alunoFormComponent.podeDesativar()){
 
       if(confirm('Quer realmente sair da página? Você perterá as alterações!')){
         this.verificaSairForm = true;
